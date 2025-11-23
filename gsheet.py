121d5da5792919f382f9c0bdd16c4081570a6f8d
shizukari-1960 <shizukari1960@gmail.com>
2025-11-23 20:52:16 +0800

    url fix

--- gsheet.py
+++ gsheet.py
@@ -4,7 +4,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from bin.bin import sheet_url
 
-from bin.bin import sheet_url
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
