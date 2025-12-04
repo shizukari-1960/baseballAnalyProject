33d0b3a775cc5494a618783617185533fdae1ed1
shizukari-1960 <shizukari1960@gmail.com>
2025-12-04 23:25:48 +0800

    fix fillNA inplace

--- selen.py
+++ selen.py
@@ -58,9 +58,8 @@ def get_table(dr: webdriver.Chrome ,team_name: str, tag_name:str, table_name: st
 
     dft = pd.read_html(table)[0]
     
-    dft.replace(np.inf, 'N/A', inplace=True)
-    dft.replace(-np.inf, 'N/A', inplace=True)
-    dft.fillna('N/A')
+    dft.replace([np.inf, -np.inf], 'N/A', inplace=True)
+    dft.fillna('N/A', inplace=True)
 
     return dft
 
